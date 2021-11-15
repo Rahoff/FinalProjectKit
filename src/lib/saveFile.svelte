5d31cea3cb96e986e4b84ac220aa35c5b142b824
Reece Hoff <rgkhoff@gmail.com>
2021-11-15 15:56:46 -0600

    Added save function

--- src/lib/saveFile.svelte
+++ src/lib/saveFile.svelte
@@ -1,6 +1,15 @@
 <script context = "module">
-    export function saveCharacter(){
-        console.log("Its verking");
+	import { saveAs } from 'file-saver';
+	import { finalName } from '$lib/stores';
+	
+    export function saveCharacter(printArea){
+		let newName;
+		finalName.subscribe( name =>{ newName = name.val });
+		var character = document.getElementById(printArea).innerText;
+		var fileName = newName + ".txt";
+		console.log(character);
+        var blob = new Blob([character], { type: "text/plain;charset=utf-8" });
+		saveAs(blob, fileName);
     }
     export function reRoll(){
 		window.location.reload(false);
